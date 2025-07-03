f414acad6972773aa72cd5e65c6b808e843eb45a
flakey5 <73616808+flakey5@users.noreply.github.com>
2025-07-03 11:18:00 -0500

    tmp

--- http2/container/main.go
+++ http2/container/main.go
@@ -17,8 +17,9 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	country := os.Getenv("CLOUDFLARE_COUNTRY_A2")
 	location := os.Getenv("CLOUDFLARE_LOCATION")
 	region := os.Getenv("CLOUDFLARE_REGION")
+	tmp := os.Getenv("CONTAINER_BLA")
 
-	fmt.Fprintf(w, "Hi, I'm a container running in %s, %s, which is part of %s ", location, country, region)
+	fmt.Fprintf(w, "Hi, I'm a container running in %s, %s, which is part of %s (tmp=%s)", location, country, region, tmp)
 }
 
 func main() {
